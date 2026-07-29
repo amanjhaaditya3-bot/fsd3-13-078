a0c813728bf18580ebc6e25aa76f7d48479c7f6b
Aditya Aman <amanjhaaditya3@gmail.com>
2026-07-29 12:15:26 +0530

    FSD is Interesting

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,4 +1,4 @@
-import { readFile, writeFile, appendFile } from "fs/promises";
+import { readFile, writeFile, appendFile} from "fs/promises";
 const writeData = async (fname,contents) => {
   await writeFile(fname,contents);
   console.log('file written');
@@ -18,7 +18,10 @@ const appendData = async (fname, contents) => {
 };
 
 
+
+
 await writeData("happy.txt", "I am Very Happy");
 await readData("happy.txt");
 await appendData("happy.txt", "FSD is Interesting");
 await readData("happy.txt");
+
