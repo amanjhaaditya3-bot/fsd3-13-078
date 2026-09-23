ca166a9aaff1c6119748b275c8da701aec78abc8
Aditya Aman <amanjhaaditya3@gmail.com>
2026-09-23 12:29:23 +0530

    hello express

---
+++ lab4/prg1.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+
+const app = express();
+// request goes here
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello EXPRESS</h1>")
+})
+
+// always listen at last
+app.listen(3333,()=>console.log("prg1 is running at 3333"));
